bbc357c5ecc988c0cb895f00cc042a00703af888
George McKinney <george@hookstream.com>
2019-10-15 03:40:02 -0700

    added fetch function to retrieve all block data every 10 seconds for the 20 most recent blocks

--- scanner/scanner.js
+++ scanner/scanner.js
@@ -2,7 +2,10 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; //SHOULD REMOVE THIS FOR PROD
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var oReq = new XMLHttpRequest();
+const fetch = require('node-fetch');
 var lastChain;
+var serverResponse;
+
 
 //https://github.com/cloudant/nodejs-cloudant
 const Cloudant = require('@cloudant/cloudant');
@@ -22,9 +25,122 @@ const cloudant = Cloudant({
     key: key,
     password: password
 });
-
 const db = cloudant.use("tgh2019"); //console.log(db); 
 
+
+//setInterval is called every 10 seconds to retrieve the last 20 blocks on the chain with all their data
+setInterval(function () {
+    findRecentChains()
+}, 10000);
+
+//
+function findRecentChains() {
+
+    // oReq.addEventListener("load", checkFoundChains);
+    // oReq.open("POST", "https://hackathon.casperlabs.io/graphql", true);
+    // oReq.send('{ "operationName": "", "variables": {}, "query": "{ dagSlice(depth: 20) { blockHash rank}}" }');
+
+    //this fetch retrieves the 20 most recent blockchain entries and stores them in a variable named json
+    //https://www.apollographql.com/docs/apollo-server/requests/
+    //does a better job handling the POST body
+    fetch("https://hackathon.casperlabs.io/graphql", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "query": "query { dagSlice(depth: 20) { \n  blockHash\n  rank\n  \n  parentHashes\n  justificationHashes\n  timestamp\n  \n  deployCount\n  validatorPublicKey\n  validatorBlockSeqNum\n  chainId\n  \n  faultTolerance\n  blockSizeBytes\n  blockSizeBytes\n  deployErrorCount\n  \n}}",
+                "operationName": "",
+                "variables": {}
+            })
+        })
+        .then(res => res.json())
+        .then(json => {
+            //  console.log(json, typeof json)
+            console.log(json.data.dagSlice.length)
+            checkFoundChains(json)
+        })
+        .catch(function (error) {
+            console.log('Request failure: ', error);
+        });
+
+    /*
+    curl 'https://hackathon.casperlabs.io/graphql' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Connection: keep-alive' -H 'DNT: 1' -H 'Origin: https://hackathon.casperlabs.io' --data-binary '{"query":"query { dagSlice(depth: 20) { \n  blockHash\n  rank\n  \n  parentHashes\n  justificationHashes\n  timestamp\n  \n  deployCount\n  validatorPublicKey\n  validatorBlockSeqNum\n  chainId\n  \n  faultTolerance\n  blockSizeBytes\n  blockSizeBytes\n  deployErrorCount\n  \n}}"}' --compressed
+    */
+
+}
+ 
+//retrieve a single document by id from the Cloudant database
+getDocumentById('74ccd506496113c7ef408ba23efbed5f018a7ebca130a0f34527ca435ff75ce0');
+function getDocumentById(chain) {
+
+    db.find({
+        selector: {
+            _id: {
+                $eq: chain
+            }
+        },
+        sort: ['_id']
+    }, function (err, doc) {
+
+        if (err) {
+            console.log("could not find document with this id");
+            return;
+        } else if (doc !== "undefined" && doc.toString().substring(0, 1) !== "u") {
+            //show us what was retrieved
+            console.log(doc);
+            // var serverResponseLength = JSON.parse(serverResponse).data.dagSlice.length;
+            // console.log(serverResponse, typeof serverResponse, new Date(), serverResponseLength);
+
+        } else {
+
+            console.log("undefined response");
+            return;
+        }
+
+    })
+}
+
+
+function checkFoundChains(serverResponse = false) {
+    var serverResponseJSON;
+    if (serverResponse === false) {
+        serverResponse = this.responseText;
+        serverResponseJSON = JSON.parse(serverResponse).data.dagSlice;
+    } else {
+      //  console.log(serverResponse.data.dagSlice, typeof serverResponse.data.dagSlice)
+        serverResponseJSON = Object.values(serverResponse.data.dagSlice);
+      //  console.log(typeof serverResponseJSON)
+    }
+    //console.log(serverResponse, typeof serverResponse, new Date());
+    const maxChain = findLastChain(serverResponseJSON);
+    console.log("maxChain", maxChain)
+
+   // process.exit(1)
+}
+
+
+function findLastChain(chains) {
+    lastChain = {
+        rank: 0,
+        blockHash: ""
+    };
+    //console.log("chains.length", chains.length)
+
+    chains.forEach(checkChain);
+
+    function checkChain(v, i, a) { //  console.log(v, v.rank, i, a);
+        if (v.rank > lastChain.rank) {
+            lastChain.rank = v.rank;
+            lastChain.blockHash = v.blockHash;
+        }
+    }
+    return lastChain;
+}
+
+
+
+/*
 function createDocument(designAndView, docId, document, template, config, debug) {
     this.designAndView = designAndView || {};
     this.docId = docId || {};
@@ -103,79 +219,4 @@ function createDocument(designAndView, docId, document, template, config, debug)
     console.log(docx_filename);
 }
 
-
-
-//setInterval
-
-//get last chain
-
-setInterval(function () {
-    findRecentChains()
-}, 10000);
-
-function findRecentChains() {
-
-    oReq.addEventListener("load", checkFoundChains);
-    oReq.open("POST", "https://hackathon.casperlabs.io/graphql", true);
-    oReq.send('{ "operationName": "", "variables": {}, "query": "{ dagSlice(depth: 20) { blockHash rank}}" }');
-
-
-}
-
-function getDocumentById(chain) {
-
-    db.find({
-        selector: {
-            _id: {
-                $eq: chain
-            }
-        },
-        sort: ['_id']
-    }, function (err, doc) {
-        
-        if (err) {
-            console.log("could not find document with this id");
-            return;
-        } else if (doc !== "undefined" && doc.toString().substring(0,1) !=="u") {
-
-            //redrawTodosUI(doc.rows);
-           console.log(doc);
-           // var serverResponseLength = JSON.parse(serverResponse).data.dagSlice.length;
-           // console.log(serverResponse, typeof serverResponse, new Date(), serverResponseLength);
-
-        } else {
-
-            console.log("undefined response");
-            return;
-        }
-
-    })
-}
-
-function findLastChain(chains) {
-    lastChain = {
-        rank: 0,
-        blockHash: ""
-    };
-    //console.log("chains.length", chains.length)
-
-    chains.forEach(checkChain);
-    function checkChain(v, i, a) {//  console.log(v, v.rank, i, a);
-        if (v.rank > lastChain.rank) {
-            lastChain.rank = v.rank;
-            lastChain.blockHash = v.blockHash;
-        }
-    }
-    return lastChain;
-}
-
-function checkFoundChains() {
-    serverResponse = this.responseText;
-    var serverResponseJSON = JSON.parse(serverResponse).data.dagSlice;
-    //console.log(serverResponse, typeof serverResponse, new Date());
-    const maxChain = findLastChain(serverResponseJSON);
-    console.log(maxChain)
-}
-var serverResponse;
-
-getDocumentById('74ccd506496113c7ef408ba23efbed5f018a7ebca130a0f34527ca435ff75ce0');
+*/
